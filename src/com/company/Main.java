e23b56ca49830c2e43cd95ce43c48c559b737ced
gracereynolds <43207494+gracereynolds@users.noreply.github.com>
2018-09-28 08:45:21 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class Main {
-    private static boolean isPalindrome(String pal)
+    public static boolean isPalindrome(String pal)
     {
         String str1 = "";
         for(int i = pal.length()-1; i >= 0; i--)
@@ -10,9 +10,16 @@ public class Main {
         }
         return str1.equals(pal);
     }
+    public static String cutOut(String str1, String cut)
+    {
+        int ind = str1.indexOf(cut);
+        return str1.substring(0,ind) + str1.substring(ind + cut.length());
+    }
     public static void main(String[] args) {
         System.out.println(isPalindrome("racecar"));
         System.out.println(isPalindrome("zara"));
+        System.out.println(cutOut("catatonic", "cat"));
+        System.out.println(cutOut("fish", "i"));
     }
 }
 
